ff1945d78a271a5333d1c4fa928841320180d5da
LanceLRQ <lancelrq@gmail.com>
2023-03-08 10:12:12 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lancelrq/deer-executor-gui
+
+go 1.19
---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from '@mui/material'
+
+function App() {
+    return (
+        <div className="deer-executor-gui">
+            Hello world
+            <Button variant="contained">Hello World</Button>
+        </div>
+    );
+}
+
+export default App;
